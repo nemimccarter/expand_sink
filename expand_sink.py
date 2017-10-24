1b7215af5754bd6cdcd2d3c4d22a50ff443f5543
nemimccarter <nemimccarter@gmail.com>
2017-10-24 06:23:02 -0700

    Switch to pandas. Print row

--- expand_sink.py
+++ expand_sink.py
@@ -1,10 +1,29 @@
 import shutil
 import os
+import pandas as pd
 
-sourceDirectory = 'testDirectory/'
-destDirectory = '../test/'
+# add generated contents to existing sink materials
+# @pre:    newSink: newly-generated directory
+#                   from which to move contents
+#       masterSink: destination folder containing
+#                   all sink materials
 
-files = os.listdir(sourceDirectory)
+def expand_sink(newSink) :
 
-for f in files:
-        shutil.move('testDirectory/' + f, '../test')
+    dataFrame = pd.read_csv(newSink)
+    inchiSubstrates = dataFrame['Substrate InChI']
+    inchiProducts = dataFrame['Product InChI']
+
+    print("hello")
+
+    for row in dataFrame :
+        print(row)
+    
+#    newFiles = os.listdir(newSink)
+
+#    for f in newFiles:
+#        shutil.move(newSink + f, masterSink)
+
+fileName = '../RetroPath2.0/tutorial_data/violacein/res/results.csv'
+
+expand_sink(fileName)
